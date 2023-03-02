ed4a1cd74e3911e33b717957516cd2c5fdc7a0d6
GabCalayan <95249972+GabCalayan@users.noreply.github.com>
2023-03-02 11:27:03 -0500

    Added the map style

---
+++ Lab_3_Map.js
@@ -0,0 +1,10 @@
+//Adding the mapbox token from my mapbox account 
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGVpc2VuemlsbGEiLCJhIjoiY2xjcXlmaHlqMGE5eTNwbjJsZDhxODhpMSJ9.Ib4qz6M3pZ7pBDGXJr8DiA'
+
+//Adding the map itself 
+const map = new mapboxgl.Map ({
+    container: 'map',
+    style: 'mapbox://styles/heisenzilla/cleg0trsv002001s135zy7mr3',
+    center: [-79.347015, 43.651070],
+    zoom: 7,
+});
